13f9e95a52dfb440e5179f5a65836fad273cdf17
devdavid2040 <infonityventas@gmail.com>
2022-07-26 16:22:10 -0300

    first commit

--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -1,3 +1,53 @@
+
+
+/*
+https://breakingbadapi.com/api/characters
+
+[
+  {
+    "char_id": 1,
+    "name": "Walter White",
+    "birthday": "09-07-1958",
+    "occupation": [
+      "High School Chemistry Teacher",
+      "Meth King Pin"
+    ],
+    "img": "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
+    "status": "Presumed dead",
+    "nickname": "Heisenberg",
+    "appearance": [
+      1,
+      2,
+      3,
+      4,
+      5
+    ],
+    "portrayed": "Bryan Cranston",
+    "category": "Breaking Bad",
+    "better_call_saul_appearance": [
+      
+    ]
+  },
+  { ... */
+
+
+/* const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('character', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+
+  });
+};
+ */
+
+
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
@@ -8,5 +58,7 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+
   });
 };
